407dbde3e1f726b5ccb0777069c081a8fd056fc8
Bryan Richter <bryan@haskell.foundation>
2025-10-10 18:22:24 +0300

    Hook in timescaledb
    Drop the noisy, unneeded zfs input.

--- modules/monitoring.nix
+++ modules/monitoring.nix
@@ -1,3 +1,8 @@
+{ config, ...}:
+let
+  dbName = "telegraf";
+  dbUser = "telegraf";
+in
 {
   services.telegraf.enable = true;
   services.telegraf.extraConfig = {
@@ -6,12 +11,9 @@
       # Interesting plugins:
       # - nstat (kernel network stats)
       # - smart (S.M.A.R.T.)
-      # - zfs
       # - disk, diskio (maybe irrelevant with zfs)
       # - nginx (accepts, active, handled, reading, requests, waiting, writing)
-      # - cpu
       # - linux_cpu (scaling and throttling)
-      # - mem
       # - exec, execd (run a given command and read its output)
       # - net
       # - netstat
@@ -20,11 +22,32 @@
       # - postgresql
       #
       # Let's start with zfs, cpu, and mem
+      # Update: zfs is just arc stats and other low-level things I don't care
+      # about right now. Dropping.
       cpu = {};
       mem = {};
-      zfs = {};
     };
 
-    outputs.discard = {};
+    outputs.postgresql = {
+      connection = "dbname=${dbName}";
+      timestamp_column_type = "timestamp with time zone";
+      create_templates = [
+        "CREATE TABLE {{ .table }} ({{ .columns }})"
+        "SELECT create_hypertable({{ .table|quoteLiteral }}, by_range('time', INTERVAL '1 week'), if_not_exists := true)"
+      ];
+    };
+  };
+  services.postgresql = {
+    enable = true;
+    ensureDatabases = [ dbName ];
+    ensureUsers = [
+      {
+        name = dbUser;
+        ensureDBOwnership = true;
+      }
+    ];
+    # Unemcumbered Apache-licensed version.
+    extensions = ps: [ ps.timescaledb-apache ];
+    settings.shared_preload_libraries = [ "timescaledb" ];
   };
 }
